2e800d28a2c1065e4369ea5220e798d668cfe7ae
Damian Montero <photoca@hotmail.com>
2018-03-09 01:19:30 -0500

    Code is ready for the presentation. Now for the slides

--- Dao-Pre-Hack.sol
+++ Dao-Pre-Hack.sol
@@ -6,10 +6,13 @@ contract VulnerableFundraiser{
 
     // VULNERABLE
     function withdrawAllMyCoins() {
-    uint widthdrawAmout = balances[msg.sender];
-    MaliciousWallet wallet = MaliciousWallet(msg.sender);
-    wallet.payout.value(widthdrawAmout)();
-    balances[msg.sender] = 0;   
+    
+        uint widthdrawAmout = balances[msg.sender];
+        MaliciousWallet wallet = MaliciousWallet(msg.sender);
+        wallet.payout.value(widthdrawAmout)();
+        
+        balances[msg.sender] = 0;   
+    
     }
 
     function getBalance() constant returns (uint) {
@@ -31,6 +34,7 @@ contract VulnerableFundraiser{
 contract MaliciousWallet {
 
     VulnerableFundraiser fundraiser;
+    uint recursions = 10;
     
     function MaliciousWallet(address fundraiserAddress) {
         fundraiser = VulnerableFundraiser(fundraiserAddress);
@@ -49,6 +53,16 @@ contract MaliciousWallet {
     }
     
     function payout() payable { 
+    
+        //exploit - using recursion
+        /*
+        
+        if (recursions > 0) { // Don't loop forever
+            recursions--;
+            fundraiser.withdrawAllMyCoins(); // Hey before you set my balance to 0 give me more!
+        }
+        
+        */
     
     }
     
